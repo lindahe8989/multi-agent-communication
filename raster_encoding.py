5bcbd27d48ae469f355d88f3f90a4dfe8cab703e
James Camacho <camacho.james@gmail.com>
2024-04-20 21:00:55 -0400

    Added initial raster encoding model

--- raster_encoding.py
+++ raster_encoding.py
@@ -1,45 +1,106 @@
+"""
+pip install mamba-ssm causal-conv1d>=1.2.0
+"""
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from mamba_ssm import Mamba
+from torchvision import datasets, transforms
 from tqdm.notebook import tqdm
+from collections import OrderedDict
+
 import numpy as np
-from mambabyte import MambaConfig, Mamba
-from torchvision import datasets, transforms, ops
 import matplotlib.pyplot as plt
 
-# from mamba_ssm import Mamba
+assert torch.cuda.is_available()
+device = torch.device("cuda")
 
-transform = transforms.Compose([transforms.ToTensor(), ops.Permute([1, 2, 0])])
-mnist_data = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-data_loader = torch.utils.data.DataLoader(dataset=mnist_data, batch_size=4, shuffle=True)
+to_pil = transforms.ToPILImage()
+to_tensor = transforms.ToTensor()
 
-# Display a few images
-data_iter = iter(data_loader)
-images, labels = next(data_iter)
-images = torch.tile(images, dims=(3,))
-plt.imshow(images[0])
+class ToRGB(nn.Module):
+  def forward(self, img):
+    return to_tensor(img).permute([1, 2, 0]).tile(dims=(3,))
 
-flattened_images = images[0].view(-1, 3)  # Reshape to (784, 3)
-int_pixels = (flattened_images * 256).int() 
-int_pixels = torch.clamp(int_pixels, 0, 255)
-one_hot = F.one_hot(int_pixels, 256)
+def expand_bits(x):
+  return 1 & (x.unsqueeze(-1) >> torch.arange(7, -1, -1))
 
-config = MambaConfig(
-    dim = 256,
-    depth = 3,
-    dt_rank = 16,
-    d_state = 2,
-    expand_factor = 2,
-    d_conv = 3,
-    dt_min = 0.001,
-    dt_max = 0.1,
-    dt_init = "random",
-    dt_scale = 1.0,
-    bias = False,
-    conv_bias = True,
-    pscan = True
-)
+class ToBits(nn.Module):
+  def forward(self, x):
+    x = torch.clamp((x * 256).int(), 0, 255)
+    x = expand_bits(x).flatten()
+    return F.one_hot(x, 2)
 
-model = Mamba(config)
-out = model(one_hot)
-print(out)
+batch_size = 4
+transform = transforms.Compose([ToRGB(), ToBits()])
+train_data = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+
+class Raster(nn.Module):
+  def __init__(self, input_dim=2, hidden_dim=16, output_dim=2, d_state=16, d_conv=4, expand=2, mamba_blocks=4):
+    super().__init__()
+    self.input_dim = input_dim
+    self.hidden_dim = hidden_dim
+    self.output_dim = output_dim
+    self.d_conv = d_conv
+    self.expand = expand
+    self.mamba_blocks = mamba_blocks
+    if input_dim != hidden_dim:
+      self.fc_in = nn.Linear(input_dim, hidden_dim)
+      self.fc_out = nn.Linear(hidden_dim, output_dim)
+    else:
+      self.fc_in = nn.Identity()
+      self.fc_out = nn.Identity()
+    self.layers = nn.ModuleList([
+        Mamba(d_model=hidden_dim, d_state=d_state, d_conv=d_conv, expand=expand)
+        for i in range(mamba_blocks)
+    ])
+
+  def forward(self, x, inference_params=None):
+    x = self.fc_in(x)
+    if inference_params is None:
+      inference_params = [None for i in self.layers]
+    for (layer, inf_param) in zip(self.layers, inference_params):
+      x = x + layer(x, inf_param)
+    x = self.fc_out(x)
+    return x
+
+def train_raster(raster, optim, epochs=10):
+  raster.train()
+
+  for epoch in range(epochs):
+    total_loss = 0
+    pbar = tqdm(train_loader)
+    for batch_idx, (target, labels) in enumerate(pbar):
+      target = target.float().to(device)
+      optim.zero_grad()
+
+      next_prediction = raster(target)
+      loss = F.cross_entropy(next_prediction, target)
+      
+      loss.backward()
+      optim.step()
+
+      total_loss += loss.item()
+      if batch_idx % 1 == 0:
+        pbar.set_description('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+          epoch, batch_idx * len(target), len(train_loader.dataset),
+          100. * batch_idx / len(train_loader), total_loss / (1 + batch_idx)))
+
+raster = Raster(mamba_blocks=3).to(device)
+optim = torch.optim.Adam(raster.parameters(), lr=1e-3)
+
+# Stop early, it'd take hours to actually train this and you get good results in just 30 seconds.
+train_raster(raster, optim, 10)
+
+with torch.no_grad():
+  x = next(iter(train_loader))[0][0].float()
+  print(x[..., 0].mean())
+  y = raster(x.unsqueeze(0).float().to(device))[0]
+  img = y.argmax(dim=-1).double().reshape(28, 28, 3, 8) @ (1 << torch.arange(7, -1, -1)).double().to(device)
+  img = img.int().cpu()
+  plt.imshow(img)
+  plt.show()
+  F.softmax(y, dim=-1)
